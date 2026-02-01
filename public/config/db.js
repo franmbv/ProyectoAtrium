93422483fd102d8a0b5d575665eff109b5f35d32
franmbv <frannermanuel@gmail.com>
2026-01-31 22:01:35 -0400

    Configuracion de Base de datos

---
+++ public/config/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+require('dotenv').config(); 
+
+// Configuración del Pool de conexiones según el estándar de la cátedra
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'museodeartecontemporaneo',
+    waitForConnections: true, 
+    connectionLimit: 10,       
+    queueLimit: 0              
+});
+
+pool.getConnection((error, connection) => {
+    if (error) throw error;
+    console.log('Conectado a MySQL exitosamente');
+    connection.release();
+});
+
+module.exports = pool.promise();
