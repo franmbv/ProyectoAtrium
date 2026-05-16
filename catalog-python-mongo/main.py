8954027684b6737343754dcb6e3a21ddc38acc08
GGSO-pixel <gloriasaimans29@gmail.com>
2026-05-16 17:07:42 -0400

    feat: Agregar uvicorn al main
    se agrego el uvicor y se acodo el .gitignore

--- catalog-python-mongo/main.py
+++ catalog-python-mongo/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.config.database import init_db  
+import uvicorn
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,3 +16,12 @@ app = FastAPI(
 @app.get("/")
 async def root():
     return {"message": "Hello World - Servidor activo y conectando a Mongo Atlas"}
+
+if __name__ == "main":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=3000,
+        reload=True,
+        workers=1
+    )
