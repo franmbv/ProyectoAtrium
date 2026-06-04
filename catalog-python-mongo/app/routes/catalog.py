f36d85352b40a2ef428809ee41a6c170e800bdd2
franmbv <frannermanuel@gmail.com>
2026-06-04 15:04:29 -0400

    Refactor: Eliminar catalog python

--- catalog-python-mongo/app/routes/catalog.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/catalog", #Esto hace que todos los endpoints comienzen con '/catalog'
-                    tags=["Categories"])
-
-@router.get("/")
-async def root():
-    return {"msg": "Bienvenido a Catalog"}
