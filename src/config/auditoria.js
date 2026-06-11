50b232b3631f6638652e2349f360278f012fcf91
franmbv <frannermanuel@gmail.com>
2026-06-10 23:45:09 -0400

    Actualización de puerto

--- src/config/auditoria.js
+++ src/config/auditoria.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const auditoriaApiUrl = process.env.AUDITORIA_API_URL || 'http://127.0.0.1:8000';
+const auditoriaApiUrl = process.env.AUDITORIA_API_URL || 'http://127.0.0.1:8080';
 
 /**
  * Envia un evento de auditoría al microservicio de Python.
